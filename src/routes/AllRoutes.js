894ba329dd351e2e6eeeb0ab8133431edb216392
John <johnclapper89@gmail.com>
2023-06-25 13:13:41 -0400

    added AllRoutes

---
+++ src/routes/AllRoutes.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+export const AllRoutes = () => {
+  return (
+    <div className="dark:bg-darkbg">
+        <Routes>
+            <Route></Route>
+        </Routes>
+    </div>
+  )
+}
