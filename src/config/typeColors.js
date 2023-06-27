66961120a6acee15980324d116cb105a5590e0c7
John <johnclapper89@gmail.com>
2023-06-27 10:54:25 -0400

    added dynamic color scheme based on pokemon types

---
+++ src/config/typeColors.js
@@ -0,0 +1,19 @@
+export const typeColors = {
+    bug: 'bg-emrald-700 dark:bg-emrald-900',
+    dark: 'bg-gray-700 dark:bg-gray-900',
+    dragon: 'bg-orange-600 dark:bg-orange-800',
+    electric: 'bg-yellow-300 dark:bg-yellow-500',
+    fairy: 'bg-pink-400 dark:bg-pink-600',
+    fighting: 'bg-amber-700 dark:bg-amber-950',
+    fire: 'bg-red-700 dark:bg-red-900',
+    flying: 'bg-sky-400 dark:bg-sky-600',
+    ghost: 'bg-violet-500 dark:bg-violet-700',
+    grass: 'bg-green-500 dark:bg-green-700',
+    ground: 'bg-yellow-950 dark:bg-orange-950',
+    normal: 'bg-neutral-400 dark:bg-neutral-600',
+    poison: 'bg-lime-600 dark:bg-lime-800',
+    psychic: 'bg-fuchsia-600 dark:bg-fuchsia-800',
+    rock: 'bg-stone-500 dark:bg-stone-700',
+    steel: 'bg-zinc-500 dark:bg-zinc-700',
+    water: 'bg-cyan-600 dark:bg-cyan-800'
+}
--- src/pages/PokemonDetails.js
+++ src/pages/PokemonDetails.js
@@ -1,4 +1,5 @@
 import { useFetchPokemon } from "../hooks/useFetchPokemon";
+import { typeColors } from "../config/typeColors";
 import { useParams } from "react-router-dom";
 
 import { Button, Card } from "../components";
@@ -18,36 +19,38 @@ export const PokemonDetails = () => {
     if (error){
         return <main>{error}</main>
     }
+
+    let pokemonType = pokemon.types[0].type.name;
     
   return (
     <main>
-            <div className="bg-red-500 mb-20 pb-5 rounded-full">
-              <h1 className="text-7xl text-center mt-10">{pokemon.name}</h1>
+            <div className={`${typeColors[pokemonType]} border-4 mb-20 pb-10 rounded-full`}>
+              <h1 className="text-7xl text-center mt-10">{pokemon.name.toUpperCase()}</h1>
             </div>
             <div className="grid grid-cols-3 grid-rows-3 gap-7 min-h-screen">
-                <div className="border-4 rounded-xl shadow-lg hover:shadow-xl">
+                <div className={`${typeColors[pokemonType]} border-4 rounded-xl shadow-lg hover:shadow-xl`}>
                     <h1 className="text-5xl text-center">Abilities:</h1>
 
                 </div>
-                <div className="border-4 rounded-xl row-span-2 shadow-lg hover:shadow-xl">
+                <div className={`${typeColors[pokemonType]} border-4 rounded-xl row-span-2 shadow-lg hover:shadow-xl`}>
                     <h1 className="text-5xl text-center">Stats:</h1>
                 </div>
-                <div className=" col-start-3 row-span-2 border-4 rounded-xl shadow-lg hover:shadow-xl"> 
+                <div className={`${typeColors[pokemonType]} col-start-3 row-span-2 border-4 rounded-xl shadow-lg hover:shadow-xl`}> 
                     <Card 
                       isImage={true} 
                       pokemonImage={pokemon.sprites.front_default}
-                      pokemonType={pokemon.types[0].type.name}
+                      pokemonType={pokemonType}
                     />                 
                       <div className="flex justify-around items-center mt-14">
                         <Button image={pokeball}>Catch</Button>
                         <Button image={run}>Run</Button>
                       </div>
                 </div>
-                <div className="border-4 rounded-xl shadow-lg hover:shadow-xl">
+                <div className={`${typeColors[pokemonType]} border-4 rounded-xl shadow-lg hover:shadow-xl`}>
                     <h1 className="text-5xl text-center">Evolutions:</h1>
                 </div>
    
-                <div className="border-4 rounded-xl col-span-3 shadow-lg hover:shadow-xl">
+                <div className={`${typeColors[pokemonType]} border-4 rounded-xl col-span-3 shadow-lg hover:shadow-xl`}>
                     <h1 className="text-5xl m-4">Locations:</h1>
                 </div>
 
