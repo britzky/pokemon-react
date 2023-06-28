f5ae6a8167b2d24a26e6a3793df30cd420648f17
John <johnclapper89@gmail.com>
2023-06-28 19:02:01 -0400

    updated hook to fetch moves and locations

--- src/hooks/useFetchPokemon.js
+++ src/hooks/useFetchPokemon.js
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 export const useFetchPokemon = (pokeName) => {
     const [pokemonInfo, setPokemonInfo] = useState([]);
     const [pokemonLocation, setPokemonLocation] = useState([]);
+    const [moves, setMoves] = useState([]);
+    const [moveInfo, setMoveInfo] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
     const navigate = useNavigate()
@@ -28,6 +30,22 @@ export const useFetchPokemon = (pokeName) => {
                 const locationResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${data.id}/encounters`)
                 const locationData = await locationResponse.json()
                 setPokemonLocation(locationData)
+                
+                const levelUpMoves = data.moves.filter(move => {
+                    return move.version_group_details.some(detail => {
+                        return detail.move_learn_method.name === 'level-up'
+                    })
+                })
+                setMoves(levelUpMoves);
+
+                const movePromises = levelUpMoves.map( async (move) => {
+                    const moveResponse = await fetch(move.move.url)
+                    const moveData = await moveResponse.json()
+                    return moveData
+                })
+                const movePromiseInfo = await Promise.all(movePromises);
+                setMoveInfo(movePromiseInfo)
+
             } catch(error) {
                 setError(error.message)
             } finally {
@@ -35,6 +53,7 @@ export const useFetchPokemon = (pokeName) => {
             }
         }
         getPokemonInfo();
+
     }, [pokeName])
-  return { pokemonInfo, pokemonLocation, loading, error }
+  return { pokemonInfo, pokemonLocation, loading, error, moves, moveInfo }
 }
