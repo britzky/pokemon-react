1f2673c721613925b6d38ba9bd7d5ed0e869bb61
John <johnclapper89@gmail.com>
2023-07-02 08:53:07 -0400

    adjusted background image

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,12 @@ import { typeImages } from "../assets/images/types"
 export const Card = ({ isImage, content, pokemonType, pokemonImage, pokemonName }) => {
 
     const background = typeImages[pokemonType]
+    const backgroundGradient = "linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.4))"
 
   return (
     <section className="p-2">
         {isImage ? (
-            <div style={{ backgroundImage: `url(${background})`}} className="rounded-lg bg-cover bg-black/50 bg-gradient-to-b">
+            <div style={{ backgroundImage: `${backgroundGradient}, url(${background})`}} className="rounded-lg bg-cover">
                 <img src={pokemonImage} alt={pokemonName} className="w-full" />
             </div>
         ) : (
