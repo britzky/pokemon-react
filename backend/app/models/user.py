5b39f476079fbf0623c220a169460e1e730446e4
John <johnclapper89@gmail.com>
2023-07-04 12:57:15 -0400

    finished User model

---
+++ backend/app/models/user.py
@@ -0,0 +1,37 @@
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String, nulllable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow())
+    pokemon = db.relationship(
+        'Pokemon', secondary=team, backref='trainers', lazy='dynamic'
+    )
+
+    #hash the password
+    def hash_password(self, original_password):
+        return generate_password_hash(original_password)
+    
+    # check the password
+    def check_hash_password(self, login_password):
+        return check_password_hash(self.password, login_password)
+    
+    # register user attributes
+    def from_dict(self, data):
+        self.user_name = data['user_name']
+        self.email = data['email']
+        self.password = self.hash_password(data['password'])
+
+    # update user attributes
+    def update_profile(self, data):
+        self.user_name = data['user_name']
+        self.email = data['email']
+
+    #save to db
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
