701f57a716080c2641cb67bc4e9595747b726a9a
John <johnclapper89@gmail.com>
2023-07-17 13:49:57 -0400

    finished pokemon card

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -15,10 +15,49 @@ export const PokemonCard = ({pokemon}) => {
             {PokemonIcon && <PokemonIcon height='25' width='25' small='true' />}
         </div>
         <div className="flex justify-around ">
-            <p>HP: {pokemon.hp_stat}</p>
-            <p>EXP: {pokemon.base_experience}</p>
+            <div className="flex gap-2">
+                <h4 className="font-bold">HP:</h4>
+                <p>{pokemon.hp_stat}</p>
+            </div>
+            <div className="flex gap-2">
+                <h4 className="font-bold">EXP:</h4>
+                <p>{pokemon.base_experience}</p>
+            </div>
         </div>
         <ImageCard pokemonImage={pokemon.pokemon_sprite} pokemonType={pokemonType} />
+        <div className="flex justify-around">
+            <div className="flex flex-col items-center">
+                <h4 className="font-bold">Abilities:</h4>
+                <ul>
+                {pokemon.ability.map((abl, index) => (
+                    <li key={index} className="my-4">{abl}</li>
+                ))}
+                </ul>
+            </div>
+            <div className="flex flex-col items-center">
+                <h4 className="font-bold">Stats:</h4>
+                <div className="flex gap-1">
+                    <h6>Atk:</h6>
+                    <p>{pokemon.attack_stat}</p>
+                </div>
+                <div className="flex gap-1">
+                    <h6>Def:</h6>
+                    <p>{pokemon.defense_stat}</p>
+                </div>
+                <div className="flex gap-1">
+                    <h6>Spd:</h6>
+                    <p>{pokemon.speed_stat}</p>
+                </div>
+                <div className="flex gap-1">
+                    <h6>Sp-Atk:</h6>
+                    <p>{pokemon.special_attack_stat}</p>
+                </div>
+                <div className="flex gap-1">
+                    <h6>Sp-Def</h6>
+                    <p>{pokemon.special_defense_stat}</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
