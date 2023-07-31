d2dbf715c84a74c5fedae69aaa3739b681f0d6e7
John <johnclapper89@gmail.com>
2023-07-31 09:58:27 -0400

    Fixed some spacing

--- src/pages/UsersTeam.js
+++ src/pages/UsersTeam.js
@@ -26,7 +26,7 @@ export const UsersTeam = () => {
             <h1 className="font-bold text-3xl">{localUser}'s Team</h1>
             <div className="grid grid-cols-1 md:grid-cols-3">
             {userPokemon && userPokemon.map((pokemon) => (
-              <div key={pokemon.id} className="py-7 px-2 flex-basis-100 md:flex-basis-50 lg:flex-basis-33">
+              <div key={pokemon.id} className="px-2 flex-basis-100 md:flex-basis-50 lg:flex-basis-33">
                 <PokemonCard pokemon={pokemon} onRelease={releasePokemon}/>
               </div>
               ))}
