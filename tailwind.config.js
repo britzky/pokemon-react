6695c310cfd941c19d17003b3c03c660df6b7255
John <johnclapper89@gmail.com>
2023-08-07 10:13:24 -0400

    upgraded vite, typescript and adjusted config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: 'class',
   theme: {
     fontFamily: {
